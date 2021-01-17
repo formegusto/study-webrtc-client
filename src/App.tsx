be47da618e674f1b2ef72c758c59620778e753b9
formegusto <formegusto@gmail.com>
2021-01-17 15:49:40 +0900

    example init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import HostComponent from './component/Webinar/HostComponent';
-import WebinarComponent from './component/WebinarComponent';
+import { BrowserRouter, Route } from 'react-router-dom';
+import AttendantsComponent from './component/AttendantsComponent';
+import HostComponent from './component/HostComponent';
 
 function App() {
   return (
-    <HostComponent />
+    <BrowserRouter>
+      <Route path="/host/@:room" component={HostComponent}/>
+      <Route path="/attendant/@:room" component={AttendantsComponent} /> 
+    </BrowserRouter>
   );
 }
 
